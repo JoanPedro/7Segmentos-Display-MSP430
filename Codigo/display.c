56494415294d58f5fcb263ca0ee3362bb8e91eeb
Joan Pedro <jpos.senai@gmail.com>
2020-04-06 13:58:41 -0300

    Adiciona o código finalizado

---
+++ Codigo/display.c
@@ -0,0 +1,40 @@
+	#include "msp430g2553.h"
+	void main (void)
+	{
+  		void delay(void);
+  		WDTCTL = WDTPW + WDTHOLD;// Para o WDT
+		
+  		P1DIR = 0xFF; // Seta P1DIR como Output, conectado ao display
+  		P1OUT = 0x00; // Desliga os 7 segmentos
+		
+  		while (1) // Ciclo de loop
+  		{
+  			P1OUT = 0x3F; //0
+  			delay();
+  			P1OUT = 0x06; //1
+  			delay();
+  			P1OUT = 0x5B; //2
+  			delay();
+  			P1OUT = 0x4F; //3
+  			delay();
+  			P1OUT = 0x66; //4
+  			delay();
+ 			  P1OUT = 0x6D; //5
+  			delay();
+  			P1OUT = 0x7D; //6
+  			delay();
+ 			  P1OUT = 0x07; //7
+ 	    	delay();
+  			P1OUT = 0x7F; //8
+  			delay();
+  			P1OUT = 0x6F; //9
+  			delay();	
+	    }
+
+  // Instância do delay manual 
+	void delay()
+	{
+  		int i,j;
+  		for(i=0;i<0xFFFF;i++)
+			 for(j=0;j<2;j++);
+	}
